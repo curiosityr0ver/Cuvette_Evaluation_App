b4b5fda2b8236b54f55fed29594308ea233ad09a
Ishu Mehta <mehta.ishu14@gmail.com>
2024-07-29 10:33:05 +0530

    refactor: Simplify login logic in userController.js

--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -4,18 +4,16 @@ const { generateToken } = require("../middleware/auth");
 const loginUser = () => (req, res) => {
     const { pin } = req.body;
 
-
-    authors.forEach(author => {
-        if (author.pin == pin) {
-            const token = generateToken(author);
-            return res.json({
-                message: 'User registered successfully',
-                token: token,
-                author: author.name
-            });
-        }
-    });
-    return res.json({ message: 'Invalid PIN' });
+    const author = authors.find(author => author.pin == pin);
+    if (!author) {
+        return res.json({ message: 'Invalid PIN' });
+    } else {
+        const token = generateToken(author);
+        return res.json({
+            message: 'User registered successfully',
+            token: token,
+        });
+    }
 };
 
 module.exports = {
